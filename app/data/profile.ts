4dbd716690c55e8e70d50a2b2d01970e0f00a541
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-09-03 09:36:28 +0800

    feat: Add profile data interface and sample user profile
    - Introduced a new `profile.ts` file containing an `IProfile` interface to define user profile structure.
    - Added a sample user profile with details such as name, email, bio, and movie statistics.
    - This module serves as a foundational dataset for user-related features in the application.

---
+++ app/data/profile.ts
@@ -0,0 +1,21 @@
+interface IProfile {
+    firstName: string;
+    lastName: string;
+    email: string;
+    bio: string;
+    movieWatch: string,
+    Favorites: string,
+    Reviews: string,
+}
+
+export const profile: IProfile[] = [
+    {
+        firstName: "Christian Dave",
+        lastName: "Alicaba",
+        email: "christiandave120702@gmail.com",
+        bio: "Movie enthusiast • Sci-fi lover • Explorer Vibe Coder",
+        movieWatch: "129M+",
+        Favorites: "1B+",
+        Reviews: "900M+",
+    }
+]
