120f0e233d18d9a8f95eac3fd168586c77900116
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-09-03 09:36:34 +0800

    feat: Add styles for favorites screen
    - Introduced a new `favorites.ts` stylesheet for the favorites screen.
    - Defined styles for the container, text, header bar, and header to enhance UI consistency.
    - This addition supports the visual layout of the favorites component in the application.

---
+++ app/styles/favorites.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#000',
+      paddingTop: 50,
+      paddingHorizontal: 10,
+    },
+    text: {
+      color: 'white',
+      fontSize: 20,
+      fontWeight: 'bold',
+    },
+    headerBar: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginBottom: 20,
+    },
+    header: {
+      fontSize: 26,
+      fontWeight: 'bold',
+      color: '#fff',
+      marginLeft: 10,
+    },
+  });
